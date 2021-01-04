ad4dfd2855d02db377ed758655c048d2cfe8098a
briancatraguna <briancatraguna@rocketmail.com>
2021-01-04 22:57:58 +0700

    Calculating direction vector

--- star_simulator.py
+++ star_simulator.py
@@ -8,8 +8,8 @@ de = radians(float(input("Enter the declination angle in degrees:\n")))
 roll = radians(float(input("Enter the roll angle in degrees:\n")))
 
 #FOV prompt from user
-x_fov = float(input("Enter the X - axis FOV in degrees:\n"))
-y_fov = float(input("Enter the Y - axis FOV in degrees:\n"))
+x_fov = radians(float(input("Enter the X - axis FOV in degrees:\n")))
+y_fov = radians(float(input("Enter the Y - axis FOV in degrees:\n")))
 
 #STEP 1: CONVERSION OF CELESTIAL COORDINATE SYSTEM TO STAR SENSOR COORDINATE SYSTEM
 a1 = (sin(ra)*cos(roll)) - (cos(ra)*sin(de)*sin(roll))
@@ -39,13 +39,22 @@ if all(orthogonal_check):
 else:
     print("WARNING: Matrix M is not orthogonal")
 
+#Converting to star sensor coordinate system
+x_i = (cos(ra)*sin(de))
+y_i = (sin(ra)*cos(de))
+z_i = (sin(de))
+direction_vector_celestial = np.array([[x_i],[y_i],[z_i]])
+print(f"Direction vector of stars in celestial coordinate system:\n{direction_vector_celestial}")
+
 #Search for image-able stars
 print("Reading in CSV file...\n")
 col_list = ["Star ID","RA","DE","Magnitude"]
 star_catalogue = pd.read_csv('Below_6.0_SAO.csv',usecols=col_list)
-print(star_catalogue)
 
-R = radians(sqrt((x_fov**2)+(y_fov**2))/2)
+R = sqrt((x_fov**2)+(y_fov**2))/2
 star_within_ra_range = ((ra - (R/cos(de))) <= star_catalogue['RA']) & (star_catalogue['RA'] <= (ra + (R/cos(de))))
 star_within_de_range = ((de - R) <= star_catalogue['DE']) & (star_catalogue['DE'] <= (de + R))
-print(star_within_ra_range,star_within_de_range)
+star_in_ra = star_catalogue[star_within_ra_range]
+star_in_de = star_catalogue[star_within_de_range]
+star_in_de = star_in_de[['Star ID']].copy()
+stars_within_FOV = pd.merge(star_in_ra,star_in_de,on="Star ID")
