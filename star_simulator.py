b1d34a2e1c569712b06e6308f152f44220ffc343
briancatraguna <briancatraguna@rocketmail.com>
2021-01-04 16:47:37 +0700

    Matrix M computation

--- star_simulator.py
+++ star_simulator.py
@@ -1,6 +1,20 @@
+from math import radians,sin,cos
+import numpy as np
+
 #Right ascension, declination and roll input prompt from user
-ra = round(float(input("Enter the right ascension angle in degrees:\n")))
-de = round(float(input("Enter the declination angle in degrees:\n")))
-roll = round(float(input("Enter the roll angle in degrees:\n")))
-print(type(ra),type(de),type(roll))
+ra = radians(float(input("Enter the right ascension angle in degrees:\n")))
+de = radians(float(input("Enter the declination angle in degrees:\n")))
+roll = radians(float(input("Enter the roll angle in degrees:\n")))
+
 #Conversion of celestial coordinate system to star sensor coordinate system
+a1 = (sin(ra)*cos(roll)) - (cos(ra)*sin(de)*sin(roll))
+a2 = -(sin(ra)*sin(roll)) - (cos(ra)*sin(de)*cos(roll))
+a3 = -(cos(ra)*cos(de))
+b1 = -(cos(ra)*cos(roll)) - (sin(ra)*sin(de)*sin(roll))
+b2 = (cos(ra)*sin(roll)) - (sin(ra)*sin(de)*cos(roll))
+b3 = -(sin(ra)*cos(de))
+c1 = (cos(ra)*sin(roll))
+c2 = (cos(ra)*cos(roll))
+c3 = -(sin(de))
+M = np.array([[a1,a2,a3],[b1,b2,b3],[c1,c2,c3]])
+print(M)
