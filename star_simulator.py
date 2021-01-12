1f979e93f04ccbbae0126c2ebd9ca87a25b81ea8
briancatraguna <briancatraguna@rocketmail.com>
2021-01-12 23:38:33 +0700

    Edit

--- star_simulator.py
+++ star_simulator.py
@@ -172,6 +172,7 @@ for i,(x1,y1) in enumerate(star_loc):
 for index in delete_indices:
     del magnitude_mv[index]
 
+background = np.zeros((w,l))
 
 for i in range(len(magnitude_mv)):
     x = round(l/2 + pixel_coordinates[i][0])
