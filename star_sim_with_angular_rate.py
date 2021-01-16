d96d09159b0e12be3c875fcbf51c96a46097a9a4
briancatraguna <briancatraguna@rocketmail.com>
2021-01-16 15:08:49 +0700

    Create angular rate file

--- star_sim_with_angular_rate.py
+++ star_sim_with_angular_rate.py
@@ -1,4 +1,17 @@
 import nested_function as nf
 
-img = nf.create_star_image(30,30,30)
+#User defines the initial attitude
+ra = float(input("Input right ascension:\n"))
+de = float(input("Input declination:\n"))
+roll = float(input("Input roll:\n"))
+
+#Angular rate prompt
+direction_sensor = input("Input direction:\n(X for +RA)\n(Y for +DE)\n(R for +ROLL)\n")
+omega = float(input("Input angular rate: "))
+
+#Frames per second
+fps = 30
+
+
+img = nf.create_star_image(ra,de,roll)
 nf.displayImg(img)
