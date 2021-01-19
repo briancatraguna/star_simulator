0eada9c64beb4f23f3e7f76baa752fcba0484f4a
briancatraguna <briancatraguna@rocketmail.com>
2021-01-19 15:11:48 +0700

    Added below 3 magnitude catalogue

--- star_filtering.py
+++ star_filtering.py
@@ -5,8 +5,10 @@ excel_catalogue = pd.read_excel('SAO.xlsx')
 tidy_catalogue = excel_catalogue.rename(columns = {'Unnamed: 0': 'Star ID', 'Unnamed: 1': 'RA', 'Unnamed: 2': 'DE', 'Unnamed: 3': "Magnitude"}, inplace=False)
 
 #Filtering Magnitude
-less_and_equal_6 = tidy_catalogue['Magnitude']<=6.0
+magnitude_filter = float(input("Input magnitude that you want to filter: "))
+less_and_equal_6 = tidy_catalogue['Magnitude']<=magnitude_filter
 filtered_catalogue = tidy_catalogue[less_and_equal_6]
 
 #Saving to CSV File
-filtered_catalogue.to_csv("Below_6.0_SAO.csv")
+file_name = "Below_" + str(magnitude_filter) + "_SAO.csv"
+filtered_catalogue.to_csv("filtered_catalogue/"+file_name)
