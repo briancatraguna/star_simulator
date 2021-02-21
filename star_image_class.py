0cd6bcb7477d91c0dfa45725fe3bc3173e406a90
briancatraguna <briancatraguna@rocketmail.com>
2021-02-21 21:49:18 +0700

    Added create M matrix method

--- star_image_class.py
+++ star_image_class.py
@@ -1,8 +1,39 @@
+from math import sin,cos
+import numpy as np
+
 class StarImage:
+    #Constructor
     def __init__(self,ra,de,roll):
         self.ra = ra
         self.de = de
         self.roll = roll
 
+    #Create M matrix method
+    def create_M_matrix(self):
+        """[summary]
+
+        Args:
+            ra ([int]): [right ascension of sensor center]
+            de ([int]): [declination of sensor center]
+            roll ([int]): [roll angle of star sensor]
+            method ([int]): [1 for method 1(Calculating each elements),2 for method 2(calculating rotation matrices)]
+        """
+        ra = self.ra
+        de = self.de
+        roll = self.roll
+        a1 = (sin(ra)*cos(roll)) - (cos(ra)*sin(de)*sin(roll))
+        a2 = -(sin(ra)*sin(roll)) - (cos(ra)*sin(de)*cos(roll))
+        a3 = -(cos(ra)*cos(de))
+        b1 = -(cos(ra)*cos(roll)) - (sin(ra)*sin(de)*sin(roll))
+        b2 = (cos(ra)*sin(roll)) - (sin(ra)*sin(de)*cos(roll))
+        b3 = -(sin(ra)*cos(de))
+        c1 = (cos(ra)*sin(roll))
+        c2 = (cos(ra)*cos(roll))
+        c3 = -(sin(de))
+        M = np.array([[a1,a2,a3],[b1,b2,b3],[c1,c2,c3]])
+
+        return M
+
 image = StarImage(0,0,0)
-print(image.ra)
+M = image.create_M_matrix()
+print(M)
